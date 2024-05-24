824c4b39f25cc7b14093b7f75f7164d7e447342e
mzz <3348257956@qq.com>
2024-05-24 16:15:23 +0800

    第二次修改

--- src/java/com/mzz/Main.java
+++ src/java/com/mzz/Main.java
@@ -3,5 +3,8 @@ package com.mzz;
 public class Main {
     public static void main(String[] args) {
         System.out.println("你好mzz");
+        //第二次修改
+        System.out.println("你好mzz");
+        System.out.println("你好mzz");
     }
 }
